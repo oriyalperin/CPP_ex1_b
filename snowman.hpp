9dd0e68336a71d82fc6ac6035c55a7558372b465
oriyalperin <oriya2011@gmail.com>
2021-06-15 16:59:01 +0300

    improved

--- snowman.hpp
+++ snowman.hpp
@@ -1,40 +1,41 @@
+#include <iostream>
 #include <string>
 
-namespace ariel{
-    const std::string hat1= " _===_";
-    const std::string hat2= "  ___ \n .....";
-    const std::string hat3= "   _  \n  /_\\";
-    const std::string hat4= "  ___ \n (_*_)";
-    const std::string nose1=",";
-    const std::string nose2=".";
-    const std::string nose3="_";
-    const std::string nose4=" ";
-    const std::string leye1="(.";
-    const std::string leye2="(o";
-    const std::string leye3="(O";
-    const std::string leye4="(-";
-    const std::string reye1=".)";
-    const std::string reye2="o)";
-    const std::string reye3="O)";
-    const std::string reye4="-)";
-    const std::string larm1="<";
-    const std::string larm2="\\";
-    const std::string larm3="/";
-    const std::string larm4=" ";
-    const std::string rarm1=">";
-    const std::string rarm2="/";
-    const std::string rarm3="\\";
-    const std::string rarm4=" ";
-    const std::string torso1="( : )";
-    const std::string torso2="(] [)";
-    const std::string torso3="(> <)";
-    const std::string torso4="(   )";
-    const std::string base1="( : )";
-    const std::string base2="(\" \")";
-    const std::string base3="(___)";
-    const std::string base4="(   )";
+namespace ariel
+{
+    const std::string hat1 = " _===_";
+    const std::string hat2 = "  ___ \n .....";
+    const std::string hat3 = "   _  \n  /_\\";
+    const std::string hat4 = "  ___ \n (_*_)";
+    const std::string nose1 = ",";
+    const std::string nose2 = ".";
+    const std::string nose3 = "_";
+    const std::string nose4 = " ";
+    const std::string leye1 = "(.";
+    const std::string leye2 = "(o";
+    const std::string leye3 = "(O";
+    const std::string leye4 = "(-";
+    const std::string reye1 = ".)";
+    const std::string reye2 = "o)";
+    const std::string reye3 = "O)";
+    const std::string reye4 = "-)";
+    const std::string larm1 = "<";
+    const std::string larm2 = "\\";
+    const std::string larm3 = "/";
+    const std::string larm4 = " ";
+    const std::string rarm1 = ">";
+    const std::string rarm2 = "/";
+    const std::string rarm3 = "\\";
+    const std::string rarm4 = " ";
+    const std::string torso1 = "( : )";
+    const std::string torso2 = "(] [)";
+    const std::string torso3 = "(> <)";
+    const std::string torso4 = "(   )";
+    const std::string base1 = "( : )";
+    const std::string base2 = "(\" \")";
+    const std::string base3 = "(___)";
+    const std::string base4 = "(   )";
 
     std::string snowman(int body);
 
 }
-
